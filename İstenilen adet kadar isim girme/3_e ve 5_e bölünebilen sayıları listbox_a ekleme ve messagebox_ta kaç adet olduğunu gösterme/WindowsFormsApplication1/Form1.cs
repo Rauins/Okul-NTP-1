a563fe870f2cd5815ada26c419654447cdd1c237
Rauins <145479140+Rauins@users.noreply.github.com>
2023-11-23 18:13:01 +0300

    3e ve 5e bölünebilen sayıları listboxa ekleme

---
+++ İstenilen adet kadar isim girme/3_e ve 5_e bölünebilen sayıları listbox_a ekleme ve messagebox_ta kaç adet olduğunu gösterme/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3;
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            for (sayi1 = Convert.ToInt16(textBox1.Text); sayi1 <= sayi2; sayi1++)
+            {
+                if (sayi1 % 3 == 0 && sayi1 % 5 == 0)
+                {
+                    listBox1.Items.Add(sayi1);
+                }
+                if (sayi1 == 0)
+                {
+                    listBox1.Items.Remove(0);
+                }
+               
+            }
+            sayi3 = listBox1.Items.Count;
+            MessageBox.Show("3'e ve 5'e bölünebilen sayıların adedi: " +sayi3);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
