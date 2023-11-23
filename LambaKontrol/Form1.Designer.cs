b04bfb8bf0dae58f1acb36386b0157c90bb3c487
Rauins <145479140+Rauins@users.noreply.github.com>
2023-11-23 21:09:19 +0300

    Lamba Kontrol

---
+++ LambaKontrol/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace LambaKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(195, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Lambanın Durumu : ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(223, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 1;
+            label2.Text = "False";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.Location = new Point(12, 29);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(129, 29);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Lambayı aç";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gold;
+            ClientSize = new Size(314, 112);
+            Controls.Add(checkBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private CheckBox checkBox1;
+    }
+}
