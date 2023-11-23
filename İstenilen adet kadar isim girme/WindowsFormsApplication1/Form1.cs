c799118e2f4aa71493e15a3e37ec7ca666241d32
Rauins <145479140+Rauins@users.noreply.github.com>
2023-11-23 18:09:23 +0300

    İstenilen adet kadar isim girme

---
+++ İstenilen adet kadar isim girme/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int adet = Convert.ToInt16(textBox2.Text);
+            for (int i = 0; i < adet; i++)
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+        }
+    }
+}
